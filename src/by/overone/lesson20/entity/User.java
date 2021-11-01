b0e81253f0719407ecc515687fa1bf9d914c9039
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-01 21:06:12 +0300

    add fields in User

--- src/by/overone/lesson20/entity/User.java
+++ src/by/overone/lesson20/entity/User.java
@@ -1,4 +1,13 @@
 package by.overone.lesson20.entity;
 
 public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+    private String phoneNumber;
+
+    public User(){
+
+    }
 }
